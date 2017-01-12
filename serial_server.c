81e2d429ad1e5676e5659b2e27383dbd66e1bc9e
dakkyun <dakkyun@github.com>
2017-01-12 17:30:57 +0900

    nothing

--- serial_server.c
+++ serial_server.c
@@ -6,11 +6,12 @@
 #include <termios.h>
 #include <unistd.h>
 
-#define SERIAL_PORT "/dev/rfcomm4"        /* シリアルインターフェースに対応するデバイスファイル */
+#define BAUDRATE B9600
+#define SERIAL_PORT "/dev/rfcomm3"        /* シリアルインターフェースに対応するデバイスファイル */
 
 int main(int argc, char *argv[])
 {
-        int buf[2] = {0,4};               /* バッファ */
+        char buf[255];                    /* バッファ */
         int fd;                           /* ファイルディスクリプタ */
         struct termios oldtio, newtio;    /* シリアル通信設定 */
                     
@@ -18,13 +19,19 @@ int main(int argc, char *argv[])
                             
         ioctl(fd, TCGETS, &oldtio);       /* 現在のシリアルポートの設定を待避させる */
         newtio = oldtio;                  /* ポートの設定をコピー */
-        newtio.c_cflag = OPOST;             /* ポートの設定をおこなう 詳細はtermios(3)参照 */
+        newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
+        newtio.c_iflag = IGNPAR;          /* ポートの設定をおこなう 詳細はtermios(3)参照 */
+        newtio.c_oflag = 0;
         ioctl(fd, TCSETS, &newtio);       /* ポートの設定を有効にする */
-                                                
+
+        newtio.c_lflag = 0;   
+        
+        newtio.c_cc[VTIME] = 0;
+        newtio.c_cc[VMIN] = 5;
+                                             
       //read(fd, buf, sizeof(buf));       /* デバイスから2バイト読み込み */
-        for(;;){
-            write(fd, buf, sizeof(buf));  /* デバイスへ2バイト書き込み */
-        }
+        buf[0] = 123;
+            write(fd, buf, 1);            /* デバイスへ1バイト書き込み */
                                                             
         ioctl(fd, TCSETS, &oldtio);       /* ポートの設定を元に戻す */
         close(fd);                        /* デバイスのクローズ */
